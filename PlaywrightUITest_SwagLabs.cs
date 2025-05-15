9bd9343abedc3e2d28fa374ddd1d3586f2d44eea
Sritaj Kumar Patel <sritajpatel@outlook.com>
2025-05-15 19:17:56 +0530

    Added playwright test script for Saunce Labs home pag

---
+++ PlaywrightUITest_SwagLabs.cs
@@ -0,0 +1,30 @@
+using Microsoft.Playwright;
+
+namespace learn_Playwright_CSharp_2025;
+
+public class PlaywrightUiTestSwagLabs
+{
+    [Test]
+    public async Task ValidateSwagLabsTitle()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+        
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://www.saucedemo.com/");
+
+        var pageTitle = page.Locator(".login_logo");
+
+        var actualTitle = await pageTitle.TextContentAsync();
+        
+        Console.WriteLine(actualTitle);
+        
+        Assert.That(actualTitle, Is.Not.Null.Or.Empty);
+        Assert.That(actualTitle, Is.EqualTo("Swag Labs"));
+
+
+    }
+}
